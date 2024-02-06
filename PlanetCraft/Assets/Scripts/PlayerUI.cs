2268ead69b43d1fd21d972ce0b75fdd15857a46f
ChaloupkaJan <chaloupkaj.06@spst.eu>
2024-02-06 19:13:56 +0100

    scene division
    Rozdělení scén, pokus o něco

---
+++ PlanetCraft/Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    /*public GameObject WorkerButton;
+    public GameObject MarineButton;
+    private void OnMouseDown()
+    {
+        if (gameObject.name == "Inhibitor")
+        {
+            WorkerButton.SetActive(true);
+        }
+        else if (gameObject.name == "Barracks")
+        {
+            MarineButton.SetActive(true);
+        }
+        else
+        {
+            WorkerButton.SetActive(false);
+            MarineButton.SetActive(false);
+        }
+    }*/
+}
